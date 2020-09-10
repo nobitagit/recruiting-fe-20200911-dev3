28a1cc67fe3f1f71e2be30ffaae2b8e89320eafd
Niels Heckmann <26061224+niels-he@users.noreply.github.com>
2020-09-10 12:19:52 +0200

    updated data fetching according to api changes

--- src/components/Inbox.js
+++ src/components/Inbox.js
@@ -1,45 +1,51 @@
 import React, { Component } from 'react';
-import api from '../api'
+import api from '../api';
 import '../Inbox.css';
 import Message from './Message';
 import ActionButton from './ActionButton';
 
 class Inbox extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       messages: null,
-      indexActiveMessage: 0
-    }
+      indexActiveMessage: 0,
+    };
   }
 
-  nextMessage = (e) => {
-    e.preventDefault()
+  nextMessage = e => {
+    e.preventDefault();
     this.setState({
-      indexActiveMessage: (this.state.indexActiveMessage + 1) % this.state.messages.length
-    })
-  }
+      indexActiveMessage:
+        (this.state.indexActiveMessage + 1) % this.state.messages.length,
+    });
+  };
 
-  previousMessage = (e) => {
-    e.preventDefault()
-    const numberOfMessages = this.state.messages.length
+  previousMessage = e => {
+    e.preventDefault();
+    const numberOfMessages = this.state.messages.length;
     this.setState({
-      //To make modulo operator works properly in case of negative number i.e. so that -1 % 3 = 2. 
-      indexActiveMessage: ((((this.state.indexActiveMessage - 1) % numberOfMessages) + numberOfMessages) % numberOfMessages) 
-    })
-  }
- 
+      //To make modulo operator works properly in case of negative number i.e. so that -1 % 3 = 2.
+      indexActiveMessage:
+        (((this.state.indexActiveMessage - 1) % numberOfMessages) +
+          numberOfMessages) %
+        numberOfMessages,
+    });
+  };
+
   componentDidMount() {
-    api.fetchInboxMessages()
-      .then(res => {this.setState({messages: res.data})})
-      .catch(err => console.log("DEBUG Error at fetchInboxMessage", err))
+    api
+      .fetchInboxMessages()
+      .then(res => {
+        this.setState({ messages: res });
+      })
+      .catch(err => console.log('DEBUG Error at fetchInboxMessage', err));
   }
 
   render() {
-  
     return (
       <div className="Inbox">
-        <div className="inbox-row">        
+        <div className="inbox-row">
           <span className="title">Inbox</span>
           <div className="inbox-buttons">
             <button onClick={this.previousMessage}>
@@ -51,7 +57,7 @@ class Inbox extends Component {
           </div>
         </div>
 
-        <Message 
+        <Message
           messages={this.state.messages}
           indexActiveMessage={this.state.indexActiveMessage}
         />
@@ -60,7 +66,6 @@ class Inbox extends Component {
           messages={this.state.messages}
           indexActiveMessage={this.state.indexActiveMessage}
         />
-      
       </div>
     );
   }
